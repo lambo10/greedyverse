109f4f020a288f8143708c7fad92f71663635c30
Lambert Nnadi <40530031+lambo10@users.noreply.github.com>
2023-02-08 09:39:43 +0100

    new commit

--- src/home/Home.js
+++ src/home/Home.js
@@ -5,6 +5,7 @@ import VideoSection from "./videoSection";
 import AboutSection1 from "./aboutSection1";
 import AboutSection2 from "./aboutSection2";
 import AboutSection3 from "./aboutSection3";
+import KycAuditSection from "./Kyc_audit_section";
 import JoinDiscordSection from "./joinDiscordSection";
 import NftDisplaySection from "./nftDisplaySection";
 import RoadMapSection from "./RoadMapSection";
@@ -12,12 +13,19 @@ import GetStarted from "./getStarted";
 import Faq from "./faq";
 import Footer from "./footer";
 import PrivateSaleSection from "./PrivateSaleSection";
+import PrivateSaleSectionNotStarted from "./privateSaleSectionNotStarted";
 import NavigationBar from "./NavigationBar";
 import TokenNomics from "./TokenNomicsSection";
 import Team from "./Team";
 import BrandsBanner from "./BrandsBanner";
 import Presale from "../components/presale/Presale";
 import UnderDevelopmentSection from "./UnderDevelopmentSection";
+import LoadingPanel from "./loadingPanel";
+import Nav from "../Layout/Nav";
+import PoweredBy from "../home/PoweredBy";
+import OtherGamesSection from "./OtherGamesSection";
+import TrailerSection from "./TrailerSection";
+
 
 import React from "react";
 
@@ -30,27 +38,35 @@ const home = () => {
     >
       {/* <UnderDevelopmentSection/> */}
 
-      <NavigationBar />
+      <LoadingPanel />
+      <Nav />
 
       {/* <SaleNotitficationBanner /> */}
-
-      <PrivateSaleSection />
+      
+      <PrivateSaleSectionNotStarted />
+      <TrailerSection />
       <BrandsBanner />
+      <KycAuditSection />
       <PosterSection />
+      <OtherGamesSection />
 
-      {/* <VideoSection/> */}
+      <VideoSection/>
+
 
       <AboutSection1 />
       <AboutSection2 />
       <AboutSection3 />
 
+
       {/* <JoinDiscordSection/> */}
       {/* <NftDisplaySection/> */}
 
+    
       <TokenNomics />
       <RoadMapSection />
       <GetStarted />
       <Team />
+      <PoweredBy/>
       <Faq />
       <Footer />
     </div>
--- src/home/loadingPanel.js
+++ src/home/loadingPanel.js
@@ -4,33 +4,32 @@ import tokenPicture from "../img/GV_w200.png";
 import React, { useState, useEffect } from 'react';
 
 const LoadingPage = (props) => {
-    const [showLoading, setShowLoading] = useState(true);
-  
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setShowLoading(false);
-        props.onPageLoad();
-      }, 1000);
-      return () => {
-        clearTimeout(timer);
-      };
-    }, [props.onPageLoad]);
-  
-    if (!showLoading) {
-      return null;
-    }
-  
-    return (
-      <div className='lb_overlay_panel loading-page'>
-        <img
-          src={tokenPicture}
-          alt='loading icon'
-          className='loading-icon'
-        />
-      </div>
-    );
-  };
-  
-  export default LoadingPage;
-  
-  
+  const [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setShowLoading(false);
+      props.onPageLoad();
+    };
+    window.addEventListener('load', handleLoad);
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, [props.onPageLoad]);
+
+  if (!showLoading) {
+    return null;
+  }
+
+  return (
+    <div className='lb_overlay_panel loading-page'>
+      <img
+        src={tokenPicture}
+        alt='loading icon'
+        className='loading-icon'
+      />
+    </div>
+  );
+};
+
+export default LoadingPage;
